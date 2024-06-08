e112623d7a2b6f8c169c8819ad922e40d9ff8e75
Lithpy1 <ryancolepalmer@gmail.com>
2024-06-07 20:43:08 -0600

    B: Need to sort the tally count dict

--- main.py
+++ main.py
@@ -5,14 +5,30 @@ def bookintotext():
 def main():
     print(bookintotext())
     countallwords()
+    #print(charactertally())
+    print(report())
 
 def countallwords():
     booktext = bookintotext()
     words = booktext.split()
     print(len(words))
-
-#def charactertally():
     
+
+def charactertally():
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    booktext = bookintotext()
+    lowertext = booktext.lower()
+    tallycount = {}
+    for alphabet in alphabet:
+         tallycount[alphabet] = booktext.count(alphabet)
+    return tallycount
+
+def report():
+    tallycount = charactertally()
+    sortedtally = dict(sorted(tallycount.items(), key=lambda item:item[1], reverse = True))
+    #This will sort the dictionary but I can't figure out how to call the keys and values by index.
+        
+
 
 main()
 
